f6a3a76eab758f37d884c11639c821e0ae150474
anyepeter <petzyrockchendi@gmail.com>
2025-11-25 18:05:57 +0100

    Add www.customtrailerpros.com to allowed image domains
    - Fix next/image unconfigured host error
    - Remove invalid '*' wildcard (not supported by Next.js)
    - Allow images from customtrailerpros.com domain

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['res.cloudinary.com'], // Cloudinary CDN domain
+    domains: ['res.cloudinary.com', 'www.customtrailerpros.com'], // Allowed image domains
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [],
     unoptimized: false, // Set to true if having optimization issues
